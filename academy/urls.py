8fb20ecf92933d4116b06c650354ae5d25664ad7
Atabek200 <akylbekovatabek564@gmail.com>
2024-05-11 14:11:26 +0600

    dee

--- academy/urls.py
+++ academy/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('product-images/', ProductImageView.as_view(), name='product-images'),
     path('comments/', CommentView.as_view(), name='comments'),
     path('ratings/', RatingView.as_view(), name='ratings'),
-    path('MyModell/', MyModelSet.as_view(), name='MyMode'),
+    path('MyModell/', MyModelSet.as_view(), name='MyModell'),
 ]
