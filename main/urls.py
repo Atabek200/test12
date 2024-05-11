2cc1ab9bfd676c70042a391603ec2ec41aa001b4
Atabek200 <akylbekovatabek564@gmail.com>
2024-05-11 15:10:12 +0600

    dee

--- main/urls.py
+++ main/urls.py
@@ -16,3 +16,4 @@ urlpatterns = [
    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_obtain_pair'),
 ]
+
